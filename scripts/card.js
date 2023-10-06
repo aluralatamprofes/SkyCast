26e821de72f959c78734346ddc1dd884fd869204
Bruno Souza <brunoezully@gmail.com>
2023-10-06 16:17:07 -0300

    feat:salvar os dados das cidades no `localstorage`

--- scripts/card.js
+++ scripts/card.js
@@ -2,7 +2,22 @@
 const apiKey = "e87b741ac20eead7877c10d5d6d3b78d";
 
 //GUARDAR VALOR DE LAS CIUDADES CREADAS
-const createdCities = [];
+const createdCities = JSON.parse(localStorage.getItem("createdCities"));
+
+if (createdCities === undefined) {
+    createdCities = [];
+}
+
+console.log(createdCities);
+const cardContainer = document.querySelector('.card-container');
+
+window.onload = async () => {
+    if(createdCities.length != 0) {
+        for (const data of createdCities) {
+            displayCard(await createCard(data));
+         }
+    }
+};
 
 // OBTENER DADOS CLIMATICOS
 async function fetchWeatherData(cityName) {
@@ -76,134 +91,138 @@ const verificationCity = async (cityName) => {
         return;
     }
     
-    return createWeatherCard(weatherData)
+    return createCardList(weatherData)
 } 
 
 // Função para criar um novo cartão com dados climáticos e qualidade do ar
-async function createWeatherCard(weatherData) {
+async function createCardList(weatherData) {
 
     createdCities.push(weatherData);
-
-    // Seleção de elementos fora do loop
-    const cardContainer = document.querySelector('.card-container');
-
+    localStorage.setItem('createdCities', JSON.stringify(createdCities));
     cardContainer.innerHTML = '';
     //loop "for...of" é usado para garantir que as operações assíncronas sejam tratadas corretamente.
     for (const data of createdCities) {
-        console.log(data);
-        const card = document.createElement('div');
-        card.classList.add('card');
-        
-        const wrapperLeft = document.createElement('div');
-        wrapperLeft.classList.add('wrapper-left');
-
-        const infoWrapper = document.createElement('div');
-        infoWrapper.classList.add('info-wrapper');
-        
-        const detailsWrapper = document.createElement('div')
-        detailsWrapper.classList.add('details-wrapper');
-        
-        const airQualityWrapper = document.createElement('div')
-        airQualityWrapper.classList.add('details-element');
-
-        const feelsLikeWrapper = document.createElement('div')
-        feelsLikeWrapper.classList.add('details-element');
-
-        const windWrapper = document.createElement('div')
-        windWrapper.classList.add('details-element');
-
-        const visibilityWrapper = document.createElement('div')
-        visibilityWrapper.classList.add('details-element');
-        
-        // ICONO DEL CLIMA
-        const iconWrapper = document.createElement('div');
-        iconWrapper.classList.add('icon-wrapper');
-
-        const weatherIconElement = document.createElement('img')
-        weatherIconElement.classList.add('icon');
-
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
-
-        weatherIconElement.src = iconUrl;
-        weatherIconElement.alt = 'Ícone do clima';
-
-        //  ELEMENTOS DE TEXTO WRAPPER 1
-        const cityNameElement = document.createElement('h2');
-        cityNameElement.classList.add('cityName');
-        cityNameElement.textContent = data.name;
-
-        const weatherDescriptionElement = document.createElement('p');
-        weatherDescriptionElement.classList.add('weatherDescription');
-        weatherDescriptionElement.textContent = `${data.weather[0].description}`;
-
-        const temperatureElement = document.createElement('p');
-        temperatureElement.classList.add('temperature');
-        temperatureElement.textContent = `${data.main.temp} °C`;
-        
-        const tempWrapper = document.createElement('div');
-        tempWrapper.classList.add('temp-wrapper');
-        
-        const minTemperatureElement = document.createElement('p');
-        minTemperatureElement.textContent = `MIN ${data.main.temp_min} °C`;
-        
-        const maxTemperatureElement = document.createElement('p');
-        maxTemperatureElement.textContent = `MAX ${data.main.temp_max} °C`;
-        
-        
-        //  ELEMENTOS DE TEXTO WRAPPER 2
-
-        const feelsLike = document.createElement('p');
-        const feelsLikeData = `${data.main.feels_like}`;
-        feelsLike.textContent = `Sensación Térmica`;
-
-        const wind = document.createElement('p');
-        const windData = `${data.wind.speed} km/h`;
-        wind.textContent = "Viento";
-
-        const airQuality = await getAirQuality(data.coord.lat, data.coord.lon);
-
-        const airQualityElement = document.createElement('p');
-        const airQualityData= `${airQuality}`;
-        airQualityElement.textContent = `Calidad del aire`;
-
-        const visibility = document.createElement('p');
-        const visibilityData = `${data.visibility} km`;
-        visibility.textContent = `Visibilidad`;
-        
-        iconWrapper.appendChild(weatherIconElement)
-        
-        infoWrapper.appendChild(cityNameElement);
-        infoWrapper.appendChild(weatherDescriptionElement);
-        infoWrapper.appendChild(temperatureElement);
-        infoWrapper.appendChild(tempWrapper);
-        
-        tempWrapper.appendChild(minTemperatureElement);
-        tempWrapper.appendChild(maxTemperatureElement);
-
-        feelsLikeWrapper.append(feelsLike, feelsLikeData);
-        windWrapper.append(wind, windData);
-        airQualityWrapper.append(airQualityElement, airQualityData);
-        visibilityWrapper.append(visibility, visibilityData);
-
-        detailsWrapper.appendChild(feelsLikeWrapper);
-        detailsWrapper.appendChild(windWrapper);
-        detailsWrapper.appendChild(airQualityWrapper);
-        detailsWrapper.appendChild(visibilityWrapper);
-
-        wrapperLeft.appendChild(iconWrapper);
-        wrapperLeft.appendChild(infoWrapper);
-        
-        card.appendChild(wrapperLeft);
-        card.appendChild(detailsWrapper);
-
-        cardContainer.appendChild(card);
+       displayCard(await createCard(data));
     }
+}
+
+async function createCard(data) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    
+    const wrapperLeft = document.createElement('div');
+    wrapperLeft.classList.add('wrapper-left');
+
+    const infoWrapper = document.createElement('div');
+    infoWrapper.classList.add('info-wrapper');
+    
+    const detailsWrapper = document.createElement('div')
+    detailsWrapper.classList.add('details-wrapper');
+    
+    const airQualityWrapper = document.createElement('div')
+    airQualityWrapper.classList.add('details-element');
+
+    const feelsLikeWrapper = document.createElement('div')
+    feelsLikeWrapper.classList.add('details-element');
+
+    const windWrapper = document.createElement('div')
+    windWrapper.classList.add('details-element');
+
+    const visibilityWrapper = document.createElement('div')
+    visibilityWrapper.classList.add('details-element');
+    
+    // ICONO DEL CLIMA
+    const iconWrapper = document.createElement('div');
+    iconWrapper.classList.add('icon-wrapper');
+
+    const weatherIconElement = document.createElement('img')
+    weatherIconElement.classList.add('icon');
+
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+    weatherIconElement.src = iconUrl;
+    weatherIconElement.alt = 'Ícone do clima';
+
+    //  ELEMENTOS DE TEXTO WRAPPER 1
+    const cityNameElement = document.createElement('h2');
+    cityNameElement.classList.add('cityName');
+    cityNameElement.textContent = data.name;
+
+    const weatherDescriptionElement = document.createElement('p');
+    weatherDescriptionElement.classList.add('weatherDescription');
+    weatherDescriptionElement.textContent = `${data.weather[0].description}`;
+
+    const temperatureElement = document.createElement('p');
+    temperatureElement.classList.add('temperature');
+    temperatureElement.textContent = `${data.main.temp} °C`;
+    
+    const tempWrapper = document.createElement('div');
+    tempWrapper.classList.add('temp-wrapper');
+    
+    const minTemperatureElement = document.createElement('p');
+    minTemperatureElement.textContent = `MIN ${data.main.temp_min} °C`;
+    
+    const maxTemperatureElement = document.createElement('p');
+    maxTemperatureElement.textContent = `MAX ${data.main.temp_max} °C`;
+    
+    
+    //  ELEMENTOS DE TEXTO WRAPPER 2
+
+    const feelsLike = document.createElement('p');
+    const feelsLikeData = `${data.main.feels_like}`;
+    feelsLike.textContent = `Sensación Térmica`;
+
+    const wind = document.createElement('p');
+    const windData = `${data.wind.speed} km/h`;
+    wind.textContent = "Viento";
+
+    const airQuality = await getAirQuality(data.coord.lat, data.coord.lon);
+
+    const airQualityElement = document.createElement('p');
+    const airQualityData= `${airQuality}`;
+    airQualityElement.textContent = `Calidad del aire`;
+
+    const visibility = document.createElement('p');
+    const visibilityData = `${data.visibility} km`;
+    visibility.textContent = `Visibilidad`;
+    
+    iconWrapper.appendChild(weatherIconElement)
+    
+    infoWrapper.appendChild(cityNameElement);
+    infoWrapper.appendChild(weatherDescriptionElement);
+    infoWrapper.appendChild(temperatureElement);
+    infoWrapper.appendChild(tempWrapper);
+    
+    tempWrapper.appendChild(minTemperatureElement);
+    tempWrapper.appendChild(maxTemperatureElement);
+
+    feelsLikeWrapper.append(feelsLike, feelsLikeData);
+    windWrapper.append(wind, windData);
+    airQualityWrapper.append(airQualityElement, airQualityData);
+    visibilityWrapper.append(visibility, visibilityData);
+
+    detailsWrapper.appendChild(feelsLikeWrapper);
+    detailsWrapper.appendChild(windWrapper);
+    detailsWrapper.appendChild(airQualityWrapper);
+    detailsWrapper.appendChild(visibilityWrapper);
+
+    wrapperLeft.appendChild(iconWrapper);
+    wrapperLeft.appendChild(infoWrapper);
+    
+    card.appendChild(wrapperLeft);
+    card.appendChild(detailsWrapper);
+
+    return card;
+}
+
+function displayCard(card) {
+    cardContainer.appendChild(card);
 }
 
 // Adicione um evento de escuta para o botão de pesquisa
 const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', handleInputCity);
 
-export { createWeatherCard, createdCities };
+export {createdCities, createCard, displayCard };
 
